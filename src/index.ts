0908084c7e1d7b1f1159193637d912f9f2a8ee57
jsus <esseswann@gmail.com>
2023-01-19 20:48:22 +0300

    Initial serverless deployment attempt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
 import { Handler } from '@yandex-cloud/function-types'
+import { execute, parse } from 'graphql'
+import { validateMessage } from 'graphql-ws'
+import handleMessage from './handleGraphqlWsMessage'
+import schema from './schema'
 
-const handler: Handler.ApiGateway.WebSocket.Message = () => {
+export const handler: Handler.ApiGateway.WebSocket.Message = (event) => {
+  let message: ReturnType<typeof validateMessage>
+  try {
+    const parsed = JSON.parse(event.body)
+    message = validateMessage(parsed)
+  } catch (error) {
+    return {
+      statusCode: 400
+    }
+  }
+
+  const contextValue = { connectionId: event.requestContext.connectionId }
+
+  const payload = handleMessage(message, async (payload) =>
+    execute({
+      schema,
+      contextValue,
+      document: parse(payload.query),
+      variableValues: payload.variables,
+      operationName: payload.operationName
+    })
+  )
+
   return {
-    statusCode: 200
+    statusCode: 200,
+    body: JSON.stringify(payload),
+    headers: {
+      'Content-Type': 'application/json'
+    }
   }
 }
