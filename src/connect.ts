eb2bf0c2fde397fefd9323f56720a180bc0b53bd
jsus <esseswann@gmail.com>
2023-02-03 19:23:19 +0300

    Connect implementaion

--- src/connect.ts
+++ src/connect.ts
@@ -2,5 +2,18 @@ import { Handler } from '@yandex-cloud/function-types'
 
 export const handler: Handler.ApiGateway.WebSocket.Connect = async (event) => {
   const connectionId = event.requestContext.connectionId
-  return { statusCode: 200 }
+  const protocol = event.headers[wsProtocolHeader.key]
+  console.log(JSON.stringify({ connectionId, protocol }))
+  return {
+    statusCode: 200,
+    headers: {
+      // This is critical for correct behavior of GraphQL Subscriptions clients
+      [wsProtocolHeader.key]: wsProtocolHeader.value
+    }
+  }
+}
+
+const wsProtocolHeader = {
+  key: 'Sec-Websocket-Protocol',
+  value: 'graphql-transport-ws'
 }
